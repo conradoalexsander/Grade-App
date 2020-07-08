6d1724ff3160b3f5066b454bb3e069acd4c04271
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-07-08 01:08:00 -0300

    Fixed baseURL endpoint, removed port

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://intense-falls-34397.herokuapp.com:3333',
+  baseURL: 'https://intense-falls-34397.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
